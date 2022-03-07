d37b1b2f7502a81f2b9a3dc5aaf9407623d84b53
Joao <joao_to10@hotmail.com>
2022-03-07 20:50:43 -0300

    Atualizaçao

--- validator.js
+++ validator.js
@@ -2,16 +2,25 @@ const nameInput = document.querySelector("#name")
 const CPFInput = document.querySelector("#CPF")
 const dateInput = document.querySelector("#date")
 const pesoInput = document.querySelector("#peso")
+const AlturaInput = document.querySelector("#Altura")
+const EstadoInput = document.querySelector("#Estado")
+const clienteInput = document.querySelector("#cliente")
 const submitButton = document.querySelector("#btn-submit")
 const erroMessage = document.querySelector(".msg")
+
+const items = document.querySelector(".items")
+
 
 submitButton.addEventListener("click", (e) =>{
   e.preventDefault()
 
   const nameValue = nameInput.value;
   const CPFValue = CPFInput.value;
-  const dateValue = dateInput.value
-const pesoValue = pesoInput.value
+  const dateValue = dateInput.value;
+const pesoValue = pesoInput.value;
+const AlturaValue = AlturaInput.value;
+const EstadoValue = EstadoInput.value;
+const clienteValue = clienteInput.value;
 
 
 if(nameValue === "" ||  CPFValue === "" || dateValue === "" || pesoValue === "" ){
@@ -19,8 +28,17 @@ if(nameValue === "" ||  CPFValue === "" || dateValue === "" || pesoValue === "" 
 
   setTimeout(() => {
     erroMessage.textContent = "";
-  }, 1000);
+  }, 3000);
   return;
 
 }
+
+const li = document.createElement('li')
+
+  li.classList = "item";
+  li.innerHTML = `Nome:${nameValue}<br />CPF:${CPFValue}<br />Data:${dateValue}<br />Peso:${pesoValue}<br /> Altura:${AlturaValue}<br /> Estado:${EstadoValue}<br /> Cadastro:${clienteValue}`
+
+
+items.appendChild(li);
+
 })
